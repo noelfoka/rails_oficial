674a0eabc543029f614d086403cbd8ed4c775b84
noelfoka <nomgnenoel@gmail.com>
2023-11-25 06:53:27 +0100

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,14 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
